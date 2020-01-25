9f7167709adac0d89d97be57894c1edd9a406e74
Acer <lp.mikymike2000@gmail.com>
2020-01-25 17:56:03 +0600

    all

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("SSS");
+        isCallingForMe();
+    }
+    public static void isCallingForMe(){
+        System.out.print("My Knee Hurts");
     }
 }
