5750681e25f22244e61d49a31e2a8e75b9cbb2e9
a.yertuyak <a.yertuyak@viledgroup.kz>
2020-02-01 15:56:18 +0600

    + add new class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("SSS");
+        System.out.println("SSSSSSSS");
+        System.out.println("NEW ISSUES");
     }
 }
