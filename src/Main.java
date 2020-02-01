36d94c57c114a04bd1c7d03aad7313155052212e
Mike <Mike@gmail.com>
2020-02-01 15:58:09 +0600

    all

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("SSS");
         isCallingForMe();
+        System.out.print("\nMiras");
     }
     public static void isCallingForMe(){
         System.out.print("My Knee Hurts");
