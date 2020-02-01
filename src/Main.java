0ae5dc67b96b44a7c86e0049aff232c8f8179cb2
Elvira <ailachi.f@gmail.com>
2020-02-01 15:57:08 +0600

    + add output "Elvira"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("SSS");
-        System.out.println("Elvira");
+        System.out.println("Elvira!!!!!");
     }
 }
