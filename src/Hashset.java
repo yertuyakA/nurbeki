ebda83999223ad1de949c57d7fcea9dc4bb8c9b5
Dragllen <kozenio06@gmail.com>
2020-02-01 16:27:19 +0600

    wewe

---
+++ src/Hashset.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+public class Hashset {
+
+    /*public static void main(String[] args) {
+
+    Set - множество - это такой же способ хранения данных, как массив или список.
+    Но особенность множества в том, что оно может хранить только уникальные значения.
+    HashSet хранит элементы в произвольном порядке, но зато быстро ищет. Подходит, если порядок Вам не важен, но важна скорость.
+    Более того, для оптимизации поиска, HashSet будет хранить элементы так, как ему удобно.
+    */
+    /* HashSet<String> myHashSet = new HashSet<String>();
+ myHashSet.add("eererer");
+ myHashSet.add("ewewe");
+ myHashSet.add("sdsdd");
+ myHashSet.add("sdsdd");
+ myHashSet.add("regvfdv");
+
+ //myHashSet.remove("sdsdd");
+ for (String i : myHashSet)
+     System.out.println(i);
+
+ //System.out.println("Does myHashSet contain 'ewewe'? " + myHashSet.contains("ewewe"));
+ //System.out.println("Now myHashSet size is " + myHashSet.size());
+ //myHashSet.clear();
+ //System.out.println("After clear myHashSet size is " + myHashSet.size());
+ //System.out.println(myHashSet.isEmpty());
+}
+}
+*/
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("SSS");
+        System.out.println("I AM GHTHTHTHFVF");
     }
+
 }
